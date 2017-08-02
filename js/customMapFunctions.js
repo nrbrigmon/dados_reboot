5c9acbfbd43d446436fbc2d60b04a65f8fb7b364
Nathan Brigmon <nathanbrigmon@Nathans-MacBook-Pro.local>
2017-08-02 14:12:05 -0500

    test2

--- js/customMapFunctions.js
+++ js/customMapFunctions.js
@@ -1,3 +1,8 @@
+/*
+TODO
+1. Min and Max should be text values like "More Educated" or "Leans Male"
+2. Add labels ONTOP of layers (see http://leafletjs.com/examples/map-panes/example.html) z-index assistance
+*/
 jQuery(document).ready(function($) {
   ///section for select2 dropdown components.
   'use strict';
@@ -44,54 +49,51 @@ jQuery(document).ready(function($) {
   basemap1.addTo(map);
 
   /* UTILITY FUNCTIONS*/
-    var toTitleCase = function(str) {
-      return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      });
-    };
+  var toTitleCase = function(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
 
-    var toCurrency = function(num) {
-      var txt = parseFloat(num).toFixed(2).toString();
-      if (txt.length > 5) {
-        return '$' + parseFloat(num).toFixed(0).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  var toCurrency = function(num) {
+    var txt = parseFloat(num).toFixed(2).toString();
+    if (txt.length > 5) {
+      return '$' + parseFloat(num).toFixed(0).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    } else {
+      return '$' + parseFloat(num).toFixed(2).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    }
+  }
+
+  var toPercent = function(num) {
+    var a = parseFloat(num * 100).toFixed(1);
+    return a + "%";
+  }
+
+  var _valueConversion = function(label, value) {
+    // console.log(label);
+    var tempLabel = _fullMetricLookup[label];
+    //get metric text
+    // if contains   , "poverty rate,", "", "% of Total", , projected change
+    if (tempLabel.indexOf("Growth") !== -1 || tempLabel.indexOf("Poverty Rate") !== -1 || tempLabel.indexOf("Percent:") !== -1 || tempLabel.indexOf("%") !== -1) {
+      //convert number depending on type
+      if (value === 0 || value === null || value === "") {
+        return "Insuff. Data"
       } else {
-        return '$' + parseFloat(num).toFixed(2).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        return toPercent(value);
       }
-
-    }
-
-    var toPercent = function(num) {
-      var a = parseFloat(num * 100).toFixed(1);
-      return a + "%";
-    }
-
-    var _valueConversion = function(label, value) {
-      // console.log(label);
-      var tempLabel = _fullMetricLookup[label];
-      //get metric text
-      // console.log(tempLabel);
-      // if contains   , "poverty rate,", "", "% of Total", , projected change
-      if (tempLabel.indexOf("Growth") !== -1 || tempLabel.indexOf("Poverty Rate") !== -1 || tempLabel.indexOf("Percent:") !== -1 || tempLabel.indexOf("%") !== -1) {
-        //convert number depending on type
-        if (value === 0 || value === null || value === "") {
-          return "Insuff. Data"
-        } else {
-          return toPercent(value);
-        }
-      } else if (tempLabel.indexOf("Wage") !== -1 || tempLabel.indexOf("Earning") !== -1 || tempLabel.indexOf("Household Income") !== -1) {
-        if (value === 0 || value === null || value === "") {
-
-          return "Insuff. Data"
-        } else {
-          return toCurrency(value);
-        }
+    } else if (tempLabel.indexOf("Wage") !== -1 || tempLabel.indexOf("Earning") !== -1 || tempLabel.indexOf("Household Income") !== -1) {
+      if (value === 0 || value === null || value === "") {
+        return "Insuff. Data"
       } else {
-        value = Math.floor(value);
-        value = value.toString().replace(/(.)(?=(\d{3})+$)/g, '$1,');
-        return value;
+        return toCurrency(value);
       }
+    } else {
+      value = Math.floor(value);
+      value = value.toString().replace(/(.)(?=(\d{3})+$)/g, '$1,');
+      return value;
     }
-    //*   STYLES  *//
+  }
+  //*   color schemes  *//
   var sequentialSchemes = {
     1: ['#ffffb2', '#fecc5c', '#fd8d3c', '#f03b20', '#bd0026'],
     2: ['#ffffcc', '#a1dab4', '#41b6c4', '#2c7fb8', '#253494'],
@@ -113,27 +115,28 @@ jQuery(document).ready(function($) {
   var qualitativeSchemes = {
     1: ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a', '#ffff99', '#b15928'],
     2: ['#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5', '#ffed6f']
-};
-function highlightFeature(e) {
-  var highlightLayer = e.target;
-  // if (e.target.feature.geometry.type === 'LineString') {
-  //   highlightLayer.setStyle({
-  //     color: '#ffff00',
-  //   });
-  // } else {
-  //   highlightLayer.setStyle({
-  //     fillColor: '#ffff00',
-  //     fillOpacity: 1
-  //   });
-  // }
-  highlightLayer.openPopup();
-}
+  };
 
-L.ImageOverlay.include({
-  getBounds: function() {
-    return this._bounds;
+  function highlightFeature(e) {
+    var highlightLayer = e.target;
+    // if (e.target.feature.geometry.type === 'LineString') {
+    //   highlightLayer.setStyle({
+    //     color: '#ffff00',
+    //   });
+    // } else {
+    //   highlightLayer.setStyle({
+    //     fillColor: '#ffff00',
+    //     fillOpacity: 1
+    //   });
+    // }
+    highlightLayer.openPopup();
   }
-});
+
+  L.ImageOverlay.include({
+    getBounds: function() {
+      return this._bounds;
+    }
+  });
 
 
   function initial_style() {
@@ -148,8 +151,12 @@ L.ImageOverlay.include({
       fillColor: '#333333',
     }
   }
-  function divergingStyle(obj){
-
+  function myFillOpacity(x) {
+    if (x === null) {
+      return 0.2;
+    } else {
+      return 0.8;
+    }
   }
   var layerGroup = {};
   map.createPane('pane_saofrancisco0729');
@@ -179,7 +186,7 @@ L.ImageOverlay.include({
       },
       mouseover: highlightFeature,
     });
-    var popupContent = '<p><b>Block: </b>'+ feature.properties['LABEL']+'</p>';
+    var popupContent = '<p><b>Block: </b>' + feature.properties['LABEL'] + '</p>';
     layer.bindPopup(popupContent);
   }
 
@@ -219,10 +226,11 @@ L.ImageOverlay.include({
   measureControl.addTo(map);
 
   $(".site-selection").select2({
-    placeholder: "Select a site",
+    placeholder: "Heliopolis",
     minimumResultsForSearch: Infinity
   });
   var $siteSelect2 = $(".site-selection");
+  // $('.site-selection').val('Heliopolis').trigger("change");
   $siteSelect2.on("select2:select", function(e) {
     var siteSelect = e.params.data.element.attributes.value.value;
     if (siteSelect == 'Helio') {
@@ -246,96 +254,210 @@ L.ImageOverlay.include({
   });
   var $metricSelect2 = $(".metric-selection");
   $metricSelect2.on("select2:select", function(e) {
-
+    //qual, sequential or diverging
     var metricType = e.params.data.element.attributes.value.value;
-    var metricObj = e.params.data;
-    if (metricType == 'qual'){
-        //qualititative style
+    //label='C1568'
+    var columnLookup = (e.params.data.element.attributes.label.value).trim();
+    //> Gender - Male <
+    var selection = e.params.data.text;
+
+    var chosenPallete;
+    if (metricType == 'qual') {
+      //qualititative style
+        chosenPallete = qualitativeSchemes[Math.floor(Math.random() * 2) + 1];
+
+        layerGroup[key].eachLayer(function(layer) {
+          layer.setStyle({
+            fillColor: myQualColor(layer.feature.properties[columnLookup]),
+            fillOpacity: myFillOpacity(layer.feature.properties[columnLookup])
+          });
+          function myQualColor(x){
+              if (x){
+                  return chosenPallete[x % chosenPallete.length];
+              } else {
+                  return '#999999'
+              }
+          }
+          var qualFeature = parseInt(layer.feature.properties[columnLookup]);
+          console.log(qualFeature);
+          var customPopUp = "<p><b>Block: </b>" + layer.feature.properties['LABEL'] + "</p>" +
+            "<p><b>" + selection + ": </b>" + fullColorLookup[columnLookup][qualFeature] + "</p>";
+          layer.bindPopup(customPopUp);
+          // console.log(layer.feature.properties['LABEL']);
+          layer.on({
+            mouseout: function(e) {
+              if (typeof layer.closePopup == 'function') {
+                layer.closePopup();
+              } else {
+                layer.eachLayer(function(feature) {
+                  feature.closePopup()
+                });
+              }
+            },
+          });
+        });
+
+        $("#attribute-legend-area").hide();
     } else {
-        var columnMax = parseFloat(e.params.data.element.attributes.max.value).toFixed(2);
-        //min='184'
-        var columnMin = parseFloat(e.params.data.element.attributes.min.value).toFixed(2);
-        //breaks='4,8,27,65'
-        var columnBreaks = e.params.data.element.attributes.breaks.value;
-        //title='C1568'
-        var columnLookup = (e.params.data.element.attributes.label.value).trim();
-        //> African American - Male <
-        var selection = e.params.data.text;
+      //max='616'
+      var columnMax = parseFloat(e.params.data.element.attributes.max.value).toFixed(2);
+      //min='184'
+      var columnMin = parseFloat(e.params.data.element.attributes.min.value).toFixed(2);
+      //breaks='4,8,27,65'
+      var columnBreaks = e.params.data.element.attributes.breaks.value;
 
-        //get values, break into an array of numbers and cleanup a touch
-        columnBreaks = columnBreaks.split(",").map(function(elem, index) {
-            return parseFloat(Math.round(elem * 100) / 100).toFixed(5);
-        })
-        var chosenPallete;
-        if (metricType == 'diver') {
-            chosenPallete = divergingSchemes[Math.floor(Math.random() * 7) + 1];
-        } else {
-            chosenPallete = sequentialSchemes[Math.floor(Math.random() * 7) + 1];
+      //get values, break into an array of numbers and cleanup a touch
+      columnBreaks = columnBreaks.split(",").map(function(elem, index) {
+        return parseFloat(Math.round(elem * 100) / 100).toFixed(5);
+      })
+      if (metricType == 'diver') {
+        chosenPallete = divergingSchemes[Math.floor(Math.random() * 7) + 1];
+      } else {
+        chosenPallete = sequentialSchemes[Math.floor(Math.random() * 7) + 1];
+      }
+      layerGroup[key].eachLayer(function(layer) {
+        layer.setStyle({
+          fillColor: myFillColor(layer.feature.properties[columnLookup]),
+          fillOpacity: myFillOpacity(layer.feature.properties[columnLookup])
+
+        });
+
+        function myFillColor(x) {
+          if (x === null) {
+            return '#999999'
+          } else {
+            if (metricType == 'diver') {
+              return x >= columnMax ? chosenPallete[4] :
+                x >= columnBreaks[3] ? chosenPallete[3] :
+                x >= columnBreaks[2] ? chosenPallete[2] :
+                x >= columnBreaks[1] ? chosenPallete[1] :
+                chosenPallete[0];
+            } else {
+              return x > columnBreaks[3] ? chosenPallete[4] :
+                x > columnBreaks[2] ? chosenPallete[3] :
+                x > columnBreaks[1] ? chosenPallete[2] :
+                x > columnBreaks[0] ? chosenPallete[1] :
+                chosenPallete[0];
+            }
+          }
         }
-        layerGroup[key].eachLayer(function (layer) {
-            layer.setStyle({
-                fillColor: myFillColor(layer.feature.properties[columnLookup]),
-                fillOpacity: myFillOpacity(layer.feature.properties[columnLookup])
 
-            });
-            function myFillColor(x) {
-                if (x === null){
-                    return '#999999'
-                } else {
-                    if (metricType == 'diver') {
-                    	return x >= columnMax ? chosenPallete[4] :
-                    	       x >= columnBreaks[3] ? chosenPallete[3] :
-                    	       x >= columnBreaks[2] ? chosenPallete[2] :
-                    	       x >= columnBreaks[1] ? chosenPallete[1] :
-                    		      chosenPallete[0];
-                    } else {
-                    	return x > columnBreaks[3] ? chosenPallete[4] :
-                    	       x > columnBreaks[2] ? chosenPallete[3] :
-                    	       x > columnBreaks[1] ? chosenPallete[2] :
-                    	       x > columnBreaks[0] ? chosenPallete[1] :
-                    		      chosenPallete[0];
-                    }
-                }
+        var customPopUp = "<p><b>Block: </b>" + layer.feature.properties['LABEL'] + "</p>" +
+          "<p><b>" + selection + ": </b>" + layer.feature.properties[columnLookup] + "</p>";
+        layer.bindPopup(customPopUp);
+        // console.log(layer.feature.properties['LABEL']);
+        layer.on({
+          mouseout: function(e) {
+            if (typeof layer.closePopup == 'function') {
+              layer.closePopup();
+            } else {
+              layer.eachLayer(function(feature) {
+                feature.closePopup()
+              });
             }
-
-            function myFillOpacity(x) {
-                if (x === null){
-                    return 0.2;
-                } else {
-                    return 0.8;
-                }
-            }
-            var customPopUp = "<p><b>Block: </b>"+ layer.feature.properties['LABEL']+"</p>"+
-                              "<p><b>"+selection+": </b>"+ layer.feature.properties[columnLookup]+"</p>";
-            layer.bindPopup(customPopUp);
-            // console.log(layer.feature.properties['LABEL']);
-            layer.on({
-              mouseout: function(e) {
-                if (typeof layer.closePopup == 'function') {
-                  layer.closePopup();
-                } else {
-                  layer.eachLayer(function(feature) {
-                    feature.closePopup()
-                  });
-                }
-              },
-            });
+          },
         });
+      });
 
-        //updateLegend
-        $("#feature-info").show()
-        $("#attribute-legend-area").show();
-        $("#color-range").show();
-        $("#color-range li.max").html("Max: " + columnMax);
-        $("#color-range li.min").html("Min: " + columnMin);
-        $("#selected_column_title").html(selection);
-        $("div.colors div").each(function(index, elem) {
-            // console.log(index);
-            $(elem).css("background-color", chosenPallete[index]);
-        });
+      //updateLegend
+      $("#feature-info").show()
+      $("#attribute-legend-area").show();
+      $("#color-range").show();
+      $("#color-range li.max").html("Max: " + columnMax);
+      $("#color-range li.min").html("Min: " + columnMin);
+      $("#selected_column_title").html(selection);
+      $("div.colors div").each(function(index, elem) {
+        // console.log(index);
+        $(elem).css("background-color", chosenPallete[index]);
+      });
     }
 
   });
 
   $(".leaflet-control-layers").prepend("<h6>Base Maps</h6>")
-});
+}); //end of jQuery closure
+
+var fullColorLookup = {
+  "Q3": {
+    1: 'Autoconstrução',
+    2: 'Mutirão',
+    3: 'Cingapura',
+    4: 'Urbanization of Favelas'
+  },
+  "Q10": {
+    1: 'Original owner',
+    2: 'Son, daughter, wife of original owner',
+    3: 'Extended family of original owner',
+    4: 'Purchased from original owner',
+    5: 'Rent from the original owner',
+    6: 'Rent from a secondary owner',
+    7: 'Exchanged houses',
+    8: 'Son, daughter, wife of secondary owner',
+    9: 'Purchased from secondary owner',
+    10: 'Relative of secondary owner',
+    11: 'Purchased the land and constructed home',
+    12: 'Purchased from family member',
+    13: 'Live for free - secondary owner',
+    14: 'Resettlement via local government',
+    15: 'Rental assistance via local government'
+  },
+  "Q33": {
+    0: 'Unemployed',
+    1: 'Employed',
+    2: 'Retired',
+    3: 'Student',
+    4: 'Housewife',
+    5: 'Works out of the home',
+    6: 'Does not work / never worked',
+    7: 'Other',
+    8: 'Bico / Informal Economy',
+    9: 'Lives on a pension',
+  },
+  "Q179_1": {
+    0: 'No participation',
+    1: 'Self-builder',
+    2: 'Family member constructed home',
+    3: 'Contracted a builder',
+    4: 'Worked with a builder',
+  },
+  "Q1_3": {
+    1: 'H-Gleba K',
+    2: 'H - Heliopolis',
+    3: 'H - Piloes',
+    4: 'H - Provisao',
+    5: 'H - Gleba H',
+    6: 'H - Janio Quadras',
+    7: 'H - Petrobras',
+    8: 'H - Portuguesa',
+    9: 'H - PAM',
+    10: 'H - 120',
+    11: 'H - Lagoa',
+    12: 'H - Copa Rio',
+    13: 'H - Mina',
+    14: 'H - Mina 2',
+    15: 'H - Redondinha',
+    16: 'H - Erundina',
+    17: 'H - Cingapuras',
+    18: 'SF-Promorar Rio Claro',
+    19: 'SF-Nucleo 1A',
+    20: 'SF-Nucleo A',
+    21: 'SF-Nucleo B',
+    22: 'SF-Nucleo 5B',
+    23: 'SF-Nucleo C',
+    24: 'SF-Nucleo E',
+    25: 'SF-Nucleo F',
+    26: 'SF-Prover 1',
+    27: 'SF-Provisao 1',
+    28: 'SF-Prover 2',
+    29: 'SF-Provisao 2',
+    30: 'SF-Prover 3',
+    31: 'SF-Provisao 3',
+    32: 'SF-Prover 4',
+    33: 'SF-Provisao 4',
+    34: 'SF-Prover 5',
+    35: 'SF-Provisao 5',
+    36: 'SF-Prover 6',
+    37: 'SF-Provisao 6',
+    38: 'SF-Prover 7'
+  }
+}
